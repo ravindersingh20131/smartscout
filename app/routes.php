dcf56516b861c9b923f846676b64700551c16d81
G. Leo Fulgencio <leo@persofolio.com>
2014-10-09 18:05:54 -0400

    added user login/register routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,7 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('login', ['uses' => 'UsersController@login']);
+Route::get('logout', ['uses' => 'UsersController@logout']);
+Route::get('register', ['uses' => 'UsersController@register']);
