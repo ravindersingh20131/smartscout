e71566e8e5f3f0b405009f76591fd7840dd03950
G. Leo Fulgencio <leo@persofolio.com>
2014-10-10 12:14:31 -0400

    added route for dashboard with auth filter

--- app/routes.php
+++ app/routes.php
@@ -16,6 +16,7 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('login', ['uses' => 'UsersController@login']);
-Route::get('logout', ['uses' => 'UsersController@logout']);
-Route::get('register', ['uses' => 'UsersController@register']);
+Route::get( 'login', [ 'uses' => 'UsersController@login' ] );
+Route::get( 'logout', [ 'uses' => 'UsersController@logout' ] );
+Route::get( 'register', [ 'uses' => 'UsersController@register' ] );
+Route::get( 'dashboard' [ 'before' => 'auth', 'uses' => 'UsersController@dashboard' ] );
