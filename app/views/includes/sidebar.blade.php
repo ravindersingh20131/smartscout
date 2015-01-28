dffc6cf4a8e28f8cfc0af8c04c1b5568c98b1368
Gamaliel Fulgencio <leo.fulgencio@gmail.com>
2015-01-27 21:31:11 -0500

    added styling for badges in sidebar

--- app/views/includes/sidebar.blade.php
+++ app/views/includes/sidebar.blade.php
@@ -1,8 +1,9 @@
 	<h3>Dashboard <i class="fa fa-angle-down fa-lg"></i></h3>
 	<nav id="sidebar-nav">
 		<ul>
-			<li>item 1 <span class="badge">22</span></li>
+			<li>item 1 <span class="badge pull-right">22</span></li>
 			<li>item 2</li>
+			<li>item 3</li>
 		</ul>
 		<button class="btn btn-primary col-md-10 col-md-offset-1">New Event</button>
 	</nav>
