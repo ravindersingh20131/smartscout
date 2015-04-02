4aa95908b72b39e661e9006a6f551608703661e5
G. Leo Fulgencio <leo@persofolio.com>
2015-04-02 11:08:24 -0400

    created database seeder try/catch blocks for ravi and other users

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -10,36 +10,86 @@ class UsersTableSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-		
+
+
 		//$adminGroup = Sentry::findGroupById(1);
 
-		//create leo's user
-		$user = Sentry::createUser(array(
-    	    'email'     => 'ghost@smartscout.me',
-    	    'password'  => 'c00kies2000',
-    	    'activated' => true,
-    	    'first_name' => 'Leo',
-    	    'username' => 'humse'
-    	));
+        try {
 
-		/*if( !$adminGroup )
-			$user->addGroup($adminGroup);*/
+            $leo = Sentry::findUserByLogin('ghost@smartscout.me');
 
-		$user->save();
+        } catch( Cartalyst\Sentry\Users\UserNotFoundException $e ) {
 
-    	//create Hamilton's user
-    	$user = Sentry::createUser(array(
-    	    'email'     => 'hdaza@smartscout.me',
-    	    'password'  => 'c00kies',
-    	    'activated' => true,
-    	    'first_name' => 'Hamilton',
-    	    'username' => 'hdaza'
-    	));
+            echo "User 'Leo' was not found.. Creating user...";
 
-    	/*if( !$adminGroup )
-    		$user->addGroup( $adminGroup );*/
+            //create leo's user
+            $user = Sentry::createUser(array(
+                'email' => 'ghost@smartscout.me',
+                'password' => 'c00kies2000',
+                'activated' => true,
+                'first_name' => 'Leo',
+                'username' => 'humse'
+            ));
 
-    	$user->save();
+            /*if( !$adminGroup )
+            $user->addGroup($adminGroup);*/
+
+            $user->save();
+
+            echo "Leo's user credentials was created!";
+
+        }
+
+        try {
+
+            $hdaza = Sentry::findUserByLogin('hdaza@smartscout.me');
+
+        } catch( Cartalyst\Sentry\Users\UserNotFoundException $e ) {
+
+            echo "User 'Hamilton' was not found.. Creating user...";
+
+            //create Hamilton's user
+            $user = Sentry::createUser(array(
+                'email' => 'hdaza@smartscout.me',
+                'password' => 'c00kies',
+                'activated' => true,
+                'first_name' => 'Hamilton',
+                'username' => 'hdaza'
+            ));
+
+            /*if( !$adminGroup )
+                $user->addGroup( $adminGroup );*/
+
+            $user->save();
+
+            echo "Hamilton's user credentials was created!";
+        }
+
+        try {
+
+            $ravinder = Sentry::findUserByLogin('ravinder@smartscout.me');
+
+        } catch( Cartalyst\Sentry\Users\UserNotFoundException $e ) {
+
+            echo "User 'Ravinder' was not found.. Creating user...";
+
+            //create ravi's user
+            $user = Sentry::createUser(array(
+                'email' => 'ravi@smartscout.me',
+                'password' => 'changemenow!',
+                'activated' => true,
+                'first_name' => 'Ravinder',
+                'username' => 'ravi'
+            ));
+
+            /*if( !$adminGroup )
+                $user->addGroup( $adminGroup );*/
+
+            $user->save();
+
+            echo "Ravinder's user credentials was created!";
+
+        }
 
 		
 	}
